311973c9a5b8482895097cb47d9583991eec9c9e
Dmitriy Tikhomirov <dmitriy.tikhomirov@yadro.com>
2026-03-29 23:29:40 +0300

    remove debug

--- lua/neovim-header/header.lua
+++ lua/neovim-header/header.lua
@@ -216,7 +216,6 @@ local function apply_header(buf, start_row, end_row, lines)
 	for _, win in ipairs(windows) do
 		local cursor = vim.api.nvim_win_get_cursor(win)
 		local row, col = cursor[1], cursor[2]
-		vim.notify(tostring(row) .. " " .. tostring(col))
 		local id = vim.api.nvim_buf_set_extmark(buf, cursor_namespace, row - 1, col, {
 			right_gravity = true,
 		})
